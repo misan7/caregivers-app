40c95e5d9b96bc2c4ea0764b0665c4b023610cb1
Sergio MT <sergio.mt88@gmail.com>
2017-05-31 10:12:05 +0200

    START POINT

--- server/app.js
+++ server/app.js
@@ -2,13 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-if (process.env.NODE_ENV !== 'production') {
-	console.log('[LOCAL] configuring ENV from .env ...')
-  require('dotenv').config()
-}
+require('dotenv').load()
 
 const PORT = process.env.PORT
 
-app.use(express.static( path.join( __dirname, '../client') ) )
+app.use(express.static(path.join(__dirname, '../client')))
 
 app.listen(PORT, () => console.log(`B-) Caregivers running on PORT ${PORT}`))
